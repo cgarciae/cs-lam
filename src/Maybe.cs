927a7d8bd6005c3e8a2face4a0c2dc9038307742
cgarciae <cgarcia.e88@gmail.com>
2014-06-22 17:15:14 -0500

    Fn (Core) + Maybe + Promise

---
+++ src/Maybe.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Functional;
+
+public abstract class Maybe <A> : Functor<A> {
+
+	public abstract bool IsNothing {get;}
+	public abstract A value {get;}
+	public abstract Maybe<B> FMap<B> (Func<A,B> f);
+	public abstract Maybe<A> FMap (Func<A,A> f);
+	public abstract Maybe<A> FMap (Action<A> f);
+
+	Functor<B> Functor<A>.FMap<B> (Func<A, B> f)
+	{
+		return FMap (f);
+	}
+
+	Functor<A> Functor<A>.FMap (Func<A, A> f)
+	{
+		throw new NotImplementedException ();
+	}
+
+	Functor<A> Functor<A>.FMap (Action<A> f)
+	{
+		throw new NotImplementedException ();
+	}
+}
+
+public class Just<A> : Maybe<A> {
+	
+	A val;
+	
+	public Just (A val) {
+		this.val = val;
+	}
+	
+	public override Maybe<B> FMap <B> (Func <A, B> f)
+	{
+		return new Just<B> (f (val));
+	}
+	
+	public override Maybe<A> FMap (Func<A, A> f)
+	{
+		val = f (val);
+		return this;
+	}
+	
+	public override Maybe<A> FMap (Action<A> f)
+	{
+		f (val);
+		return this;
+	}
+	
+	public override bool IsNothing {
+		get {
+			return false;
+		}
+	}
+	
+	public override A value {
+		get {
+			return val;
+		}
+	}
+}
+
+public class Nothing<A> : Maybe<A> {
+
+	public override Maybe<B> FMap<B> (Func<A, B> f)
+	{
+		return new Nothing<B> ();
+	}
+
+	public override Maybe<A> FMap (Func<A, A> f)
+	{
+		return this;
+	}
+
+	public override Maybe<A> FMap (Action<A> f)
+	{
+		return this;
+	}
+
+	public override bool IsNothing {
+		get {
+			return true;
+		}
+	}
+
+	public override A value {
+		get {
+			return default (A);
+		}
+	}
+}
+
+public static class Maybe {
+
+	public static Maybe<A> Make<A> ( A obj ){
+		return obj == null ? new Nothing<A> () as Maybe<A> : new Just<A> (obj) as Maybe<A>;
+	}
+
+	//Maybe.Apply :: (a -> b) -> (a -> Maybe b)
+	public static Func<A,Maybe<B>> Apply<A,B> (Func<A,B> f){
+		return a => Apply (f, a);
+	}
+	//Maybe.Apply :: (a -> b) -> a -> Maybe b
+	public static Maybe<B> Apply<A,B> (Func<A,B> f, A a){
+		return Make (a).FMap (f);
+	}
+
+}
---
+++ src/Promise.cs
@@ -0,0 +1,225 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Functional {
+
+	public abstract class Promise<A,B> : Functor<B> {
+
+		public abstract Promise<A,C> FMap<C> (Func<B,C> g);
+		public abstract Promise<A,B> FMap (Func<B,B> g);
+		public abstract Promise<A,B> FMap (Action<B> g);
+
+		public abstract Promise<A,B> Resolve (A val);
+		public abstract Promise<A,B> Reject();
+
+		public abstract B value { get; }
+
+		Functor<C> Functor<B>.FMap<C> (Func<B, C> f)
+		{
+			return FMap (f);
+		}
+
+		Functor<B> Functor<B>.FMap (Func<B, B> f)
+		{
+			return FMap (f);
+		}
+
+		Functor<B> Functor<B>.FMap (Action<B> f)
+		{
+			return FMap (f);
+		}
+
+	}
+
+	public class Pending<A,B> : Promise<A,B> {
+
+		Func<A,B> f;
+
+		public Pending (Func<A,B> g) {
+			f = g;	
+		}
+
+		public override Promise<A,C> FMap<C> (Func<B, C> g)
+		{
+			return new Pending<A,C> (Fn.Compose (g, f));
+		}
+
+		public override Promise<A,B> FMap (Func<B, B> g)
+		{
+			f = Fn.Compose (g, f);
+			return this;
+		}
+
+		public override Promise<A,B> FMap (Action<B> g)
+		{
+			f = Fn.Compose (g.ToFunc (), f);
+			return this;
+		}
+
+		public override Promise<A,B> Resolve (A val)
+		{
+			return new Resolved<A,B> (f (val));
+		}
+
+		public override Promise<A,B> Reject ()
+		{
+			return new Broken<A,B> ();
+		}
+
+		public override B value {
+			get {
+				return default(B);
+			}
+		}
+	}
+
+	public class Resolved<A,B> : Promise<A,B> {
+
+		B val;
+
+		public Resolved (B value) {
+			val = value;	
+		}
+
+		public override Promise<A,C> FMap<C> (Func<B, C> g)
+		{
+			return new Resolved<A,C> (g (val));
+		}
+
+		public override Promise<A,B> FMap (Func<B, B> g)
+		{
+			val = g (val);
+			return this;
+		}
+
+		public override Promise<A,B> FMap (Action<B> g)
+		{
+			g (val);
+			return this;
+		}
+
+		public override Promise<A,B> Resolve (A val)
+		{
+			return this;
+		}
+
+		public override Promise<A,B> Reject ()
+		{
+			return this;
+		}
+
+		public override B value {
+			get {
+				return val;
+			}
+		}
+	}
+
+	public class Broken<A,B> : Promise<A,B> {
+
+		public override Promise<A,C> FMap<C> (Func<B, C> g)
+		{
+			return new Broken<A,C> ();
+		}
+
+		public override Promise<A,B> FMap (Func<B, B> g)
+		{
+			return this;
+		}
+
+		public override Promise<A,B> FMap (Action<B> g)
+		{
+			return this;
+		}
+
+		public override Promise<A,B> Resolve (A val)
+		{
+			return this;
+		}
+
+		public override Promise<A,B> Reject ()
+		{
+			return this;
+		}
+
+		public override B value {
+			get {
+				return default(B);
+			}
+		}
+	}
+
+	public interface Promise {
+		
+		Promise FMap (Action g);
+		Promise resolve ();
+		Promise reject ();
+	}
+
+	public class Pending : Promise {
+		Action f;
+
+		public Pending (Action g) {
+			f = g;
+		}
+
+		public Promise FMap (Action g)
+		{
+			f = Fn.Compose (g, f);
+			return this;
+		}
+
+		public Promise resolve ()
+		{
+			f ();
+			return new Resolved ();
+		}
+
+		public Promise reject ()
+		{
+			return new Broken ();
+		}
+	}
+
+	public class Resolved : Promise {
+
+		public Promise FMap (Action g)
+		{
+			g ();
+			return this;
+		}
+
+		public Promise resolve ()
+		{
+			return this;
+		}
+
+		public Promise reject ()
+		{
+			return this;
+		}
+
+	}
+
+	public class Broken : Promise {
+
+		public Promise FMap (Action g)
+		{
+			return this;
+		}
+
+		public Promise resolve ()
+		{
+			return this;
+		}
+
+		public Promise reject ()
+		{
+			return this;
+		}
+	}
+
+}

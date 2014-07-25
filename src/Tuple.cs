021e433e395055f690000f61b104815f31552a6d
cgarciae <cgarcia.e88@gmail.com>
2014-07-25 17:48:51 -0500

    Tuple

---
+++ src/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Tuple<A,B> : Functor<B> {
+
+	public readonly A fst;
+	public readonly B snd;
+
+	public Tuple (A a, B b) {
+		fst = a;
+		snd = b;
+	}
+
+	Functor<C> Functor<B>.FMap<C> (Func<B, C> f)
+	{
+		return new Tuple<A,C> (fst, f (snd));
+	}
+}
+
+public static partial class Fn {
+	public static Tuple<A,B> Tuple<A,B> (A a, B b) {
+		return new Tuple<A,B> (a, b);
+	}
+}
